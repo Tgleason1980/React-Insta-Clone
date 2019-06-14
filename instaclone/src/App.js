29036a818c39454db314c37b8d466dd10c94c306
Tgleason1980 <tglabon@gmail.com>
2019-06-14 14:38:27 -0500

    added app.js

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import dummyData from './dummy-data';
 import PropTypes from 'prop-types';
+import Post from '../src/components/PostContainer/Post'
 
-function App() {
+class App extends Component{
+  constructor(){
+    super();
+      this.state = {
+      data: []
+  }
+};
+componentDidMount(){
+  this.setState({data: dummyData})
+}
+render(){
   return (
-    <div className="App">
-    
+    <div className="container">
+      <Post 
+        data= {this.state.data}
+      />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
