6103e8c111ad75a51383ce71b00d1b806d5411c7
Tgleason1980 <tglabon@gmail.com>
2019-06-14 14:19:31 -0500

    new commit

--- instaclone/src/components/PostContainer/Post.js
+++ instaclone/src/components/PostContainer/Post.js
@@ -1,1 +1,22 @@
-import React from 'react'
+import React, {Component} from 'react';
+
+
+ class Post extends Component {
+  render() {
+    return (
+      <div className="posts">
+        {this.props.data.map(post =>{
+            return(
+                
+                <header>
+                    <image src={post.imageUrl}/>
+                    <h3>{post.username}</h3>
+                </header>
+            )
+        })}
+      </div>
+    );
+  }
+}
+
+export default Post;
